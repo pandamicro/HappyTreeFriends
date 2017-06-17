2e9cb50bd6defcb14cd16bbf62b47168eaf1ce73
Deniulor <417928448@qq.com>
2017-06-17 14:02:24 +0800

    controller

--- scripts/controller.js
+++ scripts/controller.js
@@ -35,71 +35,222 @@ requirejs([
     '../node_modules/happyfuntimes/dist/hft',
     '../node_modules/hft-sample-ui/dist/sample-ui',
     '../node_modules/hft-game-utils/dist/game-utils',
-  ], function(
+  ],
+  function(
     hft,
     sampleUI,
-    gameUtils) {
+    gameUtils,
+    AudioManager,
+    ImageLoader,
+    ImageProcess) {
 
   var GameClient = hft.GameClient;
-  var CommonUI = sampleUI.commonUI;
-  var Input = sampleUI.input;
-  var Misc = sampleUI.misc;
-  var MobileHacks = sampleUI.mobileHacks;
-  var Touch = sampleUI.touch;
+  var commonUI = sampleUI.commonUI;
+  var DPad = sampleUI.DPad;
+  var input = sampleUI.input;
+  var misc = sampleUI.misc;
+  var mobileHacks = sampleUI.mobileHacks;
+  var Ticker = gameUtils.Ticker;
+  var touch = sampleUI.touch;
+
+
+  var g_client;
+  var g_clock;
+  var g_grid;
+  var g_instrument;
+  var g_leftRight = 0;
+  var g_oldLeftRight = 0;
+  var g_abutton = false;
 
   var globals = {
     debug: false,
+    forceController: false,
+    orientation: "landscape-primary",
   };
-  Misc.applyUrlSettings(globals);
-  MobileHacks.fixHeightHack();
+  misc.applyUrlSettings(globals);
+  mobileHacks.fixHeightHack();
 
-  var score = 0;
-  var statusElem = document.getElementById("gamestatus");
-  var inputElem = document.getElementById("inputarea");
-  var colorElem = document.getElementById("display");
-  var client = new GameClient();
+  var $ = document.getElementById.bind(document);
 
-  CommonUI.setupStandardControllerUI(client, globals);
-  CommonUI.askForNameOnce();   // ask for the user's name if not set
-  CommonUI.showMenu(true);     // shows the gear menu
+  var bombsCtx = $("bombs").getContext("2d");
+  var bombSizeCtx = $("bombsize").getContext("2d");
+  var msgContainerStyle = $("msgcontainer").style;
+  var msgText = misc.createTextNode($("msg"));
+  var msgContainerOriginalDisplay = msgContainerStyle.display;
+  var msgTimeoutId;
 
-  var randInt = function(range) {
-    return Math.floor(Math.random() * range);
+
+  var flashNdx;
+  var ticker = new Ticker();
+  var cancelFlash = function() {
+    ticker.cancel();
   };
-
-  // Sends a move command to the game.
-  //
-  // This will generate a 'move' event in the corresponding
-  // NetPlayer object in the game.
-  var sendMoveCmd = function(position, target) {
-    client.sendCmd('move', {
-      x: position.x / target.clientWidth,
-      y: position.y / target.clientHeight,
+  var flash = function(colors) {
+    flashNdx = 0;
+    cancelFlash();
+    ticker.tick(2, 0.1, function() {
+      msgContainerStyle.backgroundColor = colors[(flashNdx++) % colors.length];
     });
   };
 
-  // Pick a random color
-  var color =  'rgb(' + randInt(256) + "," + randInt(256) + "," + randInt(256) + ")";
-  // Send the color to the game.
-  //
-  // This will generate a 'color' event in the corresponding
-  // NetPlayer object in the game.
-  client.sendCmd('color', {
-    color: color,
-  });
-  colorElem.style.backgroundColor = color;
+  var showMsg = function(msg, color, timeout) {
+    cancelFlash()
+    msgContainerStyle.backgroundColor = color;
+    msgContainerStyle.display = msgContainerOriginalDisplay;
+    msgText.nodeValue = msg;
+    if (msgTimeoutId) {
+      clearTimeout(msgTimeoutId);
+      msgTimeoutId = undefined;
+    }
+    if (timeout) {
+      msgTimeoutId = setTimeout(hideMsg, timeout * 1000);
+    }
+  };
 
-  // Send a message to the game when the screen is touched
-  inputElem.addEventListener('pointermove', function(event) {
-    var position = Input.getRelativeCoordinates(event.target, event);
-    sendMoveCmd(position, event.target);
-    event.preventDefault();
+  var hideMsg = function() {
+    msgContainerStyle.display = "none";
+  };
+
+
+  g_client = new GameClient();
+
+  var handleScore = function() {
+  };
+
+  var handleDeath = function() {
+    showMsg("DEAD!", "red");
+    flash(["red", "yellow"]);
+  };
+
+  var handleWinner = function() {
+    showMsg("WINNER!!!", "yellow");
+    flash(["green", "blue", "purple", "red", "orange", "yellow", "purple"]);
+  };
+
+  var handleTie = function() {
+    showMsg("tie", "green");
+  };
+
+  var handleWaitForStart = function(data) {
+    showMsg("Start In: " + data.waitTime, "blue");
+  };
+
+  var handleWaitForNextGame = function(data) {
+    showMsg("Please Wait For Next Game", "orange");
+  };
+
+  var handleWaitForMorePlayers = function(data) {
+    showMsg("Please Wait For More Players", "orange");
+  };
+
+  var handleStart = function() {
+    hideMsg();
+  };
+
+  var handleSpoil = function() {
+    showMsg("Take Revenge!", "green", 2);
+  };
+
+  var handleSetColor = function(msg) {
+    var canvas = $("avatar");
+    var width = canvas.clientWidth;
+    var height = canvas.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
+    var ctx = canvas.getContext("2d");
+    var frame = ImageProcess.adjustHSV(images.avatars[msg.set], msg.hsv[0], msg.hsv[1], msg.hsv[2]);
+    ctx.drawImage(frame, 0, 0);
+  };
+
+  var handleNumBombs = function(msg) {
+    misc.resize(bombsCtx.canvas);
+    bombsCtx.clearRect(0, 0, bombsCtx.canvas.width, bombsCtx.canvas.height);
+    for (var ii = 0; ii < msg.numBombs; ++ii) {
+      bombsCtx.drawImage(images.bomb, ii * 16, 0);
+    }
+  };
+
+  var handleBombSize = function(msg) {
+    misc.resize(bombSizeCtx.canvas);
+    bombSizeCtx.clearRect(0, 0, bombSizeCtx.canvas.width, bombSizeCtx.canvas.height);
+    bombSizeCtx.drawImage(images.flames[1], 0, 0);
+    for (var ii = 1; ii <= msg.size; ++ii) {
+      bombSizeCtx.drawImage(images.flames[0], ii * 16, 0);
+    }
+    bombSizeCtx.drawImage(images.flames[2], ii * 16, 0);
+  };
+
+  if (globals.forceController) {
+    hideMsg();
+  } else {
+    // These messages hide/show the controller so don't handle them
+    // if we're testing the controller with `forceController`
+    g_client.on('score', handleScore);
+    g_client.on('start', handleStart);
+    g_client.on('tied', handleTie);
+    g_client.on('died', handleDeath);
+    g_client.on('spoil', handleSpoil);
+    g_client.on('winner', handleWinner);
+    g_client.on('waitForStart', handleWaitForStart);
+    g_client.on('waitForNextGame', handleWaitForNextGame);
+    g_client.on('waitForMorePlayers', handleWaitForMorePlayers);
+  }
+  g_client.on('numBombs', handleNumBombs);
+  g_client.on('bombSize', handleBombSize);
+  g_client.on('setColor', handleSetColor);
+
+  commonUI.setupStandardControllerUI(g_client, globals);
+  commonUI.askForNameOnce();   // ask for the user's name if not set
+  commonUI.showMenu(true);     // shows the gear menu
+
+  var dpads = [
+    new DPad({element: $("dpadleft")}),
+  ];
+
+  var handleAbutton = function(pressed) {
+    if (g_abutton != pressed) {
+      g_abutton = pressed;
+      g_client.sendCmd('abutton', {
+          abutton: pressed,
+      });
+    }
+  };
+
+  var handleShow = function(pressed) {
+    g_client.sendCmd('show', {show:pressed});
+  };
+
+  var handleDPad = function(e) {
+    dpads[e.pad].draw(e.info);
+    console.log(e);
+    // g_client.sendCmd('pad', {pad: e.pad, dir: e.info.direction});
+    g_client.sendCmd('moving', {orientation: e.info.direction * 90});
+  };
+
+  var keys = { };
+  keys["Z".charCodeAt(0)] = function(e) { handleAbutton(e.pressed); }
+  keys["X".charCodeAt(0)] = function(e) { handleShow(e.pressed); }
+  input.setupKeys(keys);
+  input.setupKeyboardDPadKeys(handleDPad, input.kASWDPadOnly);
+
+  touch.setupButtons({
+    inputElement: $("buttons"),
+    buttons: [
+      { element: $("abuttoninput"), callback: function(e) { handleAbutton(e.pressed); }, },
+      { element: $("avatarinput"),  callback: function(e) { handleShow(e.pressed); }, },
+    ],
   });
 
-  // Update our score when the game tells us.
-  client.addEventListener('scored', function(cmd) {
-    score += cmd.points;
-    statusElem.innerHTML = "You scored: " + cmd.points + " total: " + score;
-  });
+  touch.setupVirtualDPads({
+    inputElement: $("dpadleftinput"),
+    callback: handleDPad,
+    fixedCenter: false,
+    deadSpaceRadius: 0,
+    divisions: 4,
+    pads: [
+      {
+        referenceElement: $("dpadleft"),
+      },
+    ],
+  })
 });
-
